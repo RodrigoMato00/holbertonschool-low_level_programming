bc108209602b48078f2b760beac6ebe458e5eb52
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2021-06-18 18:05:31 -0300

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This function print the alphabet a...z
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int aux;
+
+for (aux = 0; aux < 10; aux++)
+{
+putchar((aux % 10) + '0');
+if (aux < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
