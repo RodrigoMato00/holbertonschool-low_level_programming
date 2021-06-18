f99ed001842ad8c84a9b14596d7c0bc5661845a7
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2021-06-18 15:50:38 -0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,24 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
 /**
  *main - Entry point
- *this fuction print the alphabet a...z, A...Z
  *
- *Return: Always 0 (success)
+ *Description: This program prints all letters from a-z in lower case
+ *Return: Returns 0 upon successful completion
  */
 int main(void)
 {
-char start = 'z';
-char aux;
-for (aux = 'a'; aux <= start; aux++)
+char letter = 'a';
+
+while (letter <= 'z')
 {
-putchar(aux);
+putchar(letter);
+letter++;
 }
 putchar('\n');
-{
-putchar(aux);
-}
 return (0);
 }
