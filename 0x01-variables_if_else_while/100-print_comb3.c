c242b002eea8fa5b0ea1016cf4fc1f8ca6399e20
RodrigoMato00 <3293@holbertonschool.com>
2021-06-19 10:35:43 -0700

    low_level_programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,29 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
 /**
  * main - Entry point
  *
- * This function print the alphabet a...z
+ * This function prints all possible different combinations of two digits
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
 int aux;
+int aux2;
 
 for (aux = 0; aux < 10; aux++)
 {
+for (aux2 = 0; aux2 < 10; aux2++)
+{
+if (aux != aux2 && aux < aux2)
+{
 putchar((aux % 10) + '0');
-if (aux < 9)
+putchar((aux2 % 10) + '0');
+if (aux != 8 || aux2 != 9)
 {
 putchar(',');
 putchar(' ');
+}
+}
 }
 }
 putchar('\n');
