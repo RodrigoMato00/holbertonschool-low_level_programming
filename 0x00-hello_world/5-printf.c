39ec7ed9182d45bc7252a139c336900843c7073d
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2021-06-21 13:28:51 -0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 /**
 *main - Print a message printf
 *
-*Return: end program
+*Return: 0 if the function went through to completion successfully
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
