eaca0858938ca4a9066283cfe9fa6456facc49dd
RodrigoMato00 <3293@holbertonschool.com>
2021-06-22 09:38:24 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *main - print "holberton " include followed be a new line
+ *Description: you are not allowed to include a standar libraries
+ *Return: 0
+ */
+int  main(void)
+{
+char holberton[] = "holberton";
+int i = 0;
+while (holberton[i] != '\0')
+{
+_putchar(holberton[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
