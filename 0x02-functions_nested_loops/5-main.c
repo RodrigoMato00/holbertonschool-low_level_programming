8f417b8644556a74c08a03b1ff221f925ae009e1
RodrigoMato00 <3293@holbertonschool.com>
2021-06-22 13:46:57 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,26 +1,33 @@
 #include "holberton.h"
 
 /**
- * print_sign - Entry point
+ * main - check the code for Holberton School students.
  *
- * Description: This function print the sign of a number
- *
- * @n: number to discover the sign
- *
- * Return: 1 if n is positive, 0 is zero, -1 if is less than zero
+ * Return: Always 0.
  */
-int print_sign(int n)
+int main(void)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	_putchar('-');
-	return (-1);
+	int r;
+
+	r = print_sign(98);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(0);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(0xff);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(-1);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,31 @@
 #include "holberton.h"
+
 /**
- * print_sign - Print the sign of a number
- * @n: type int integer, can be negative or positive
- * Description: print +, 0, or - depending on number, along with return
- * Return: 1 if +, 0 if 0, and -1 if -
+ * print_sign - Entry point
+ *
+ * Description: This function print the sign of a number
+ *
+ * @n: number to discover the sign
+ *
+ * Return: 1 if n is positive, 0 is zero, -1 if is less than zero
  */
 int print_sign(int n)
 {
-if (n > 0)
-{
-_putchar('+');
-return (1);
-}
-else if (n < 0)
-{
-_putchar('-');
-return (-1);
-}
-else
-{
-_putchar('0');
-return (0);
-}
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
