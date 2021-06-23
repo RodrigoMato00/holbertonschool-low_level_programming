761bae0eb7390be6a693edddecba3dd0f65e2e72
RodrigoMato00 <3293@holbertonschool.com>
2021-06-23 10:28:16 -0700

    hola

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print 4000000 fibonacci numbers, starting with 1 and 2.
+ * Numbers must be coma and space separated.
+ * Return: 0
+ */
+int main(void)
+{
+int (i = 1; j = 2; total 0)
+int k;
+while < 4000000)
+k = j;
+j += i;
+i = k;
+}
+pritf("%d/n", total);
+return (0);
+}
