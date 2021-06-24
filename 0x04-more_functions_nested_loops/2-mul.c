6a13f8e0f0897873f9a4f53143742e0110fffee2
RodrigoMato00 <3293@holbertonschool.com>
2021-06-24 10:48:49 -0700

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiply two numbers
+ * Description - This function is to multiply two numbers
+ * @a: Integer to multiply
+ * @b: Integer to multiply
+ * Return: (c * b)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
