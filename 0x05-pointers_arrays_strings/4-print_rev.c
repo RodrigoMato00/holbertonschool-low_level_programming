647d8c2dc9e0aaff21a7450cf0a9e5149e869a5b
RodrigoMato00 <3293@holbertonschool.com>
2021-06-28 15:21:21 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_rev - print in reverse
+ * @s: char arra string
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	for (i--; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
