df616f6a170917e21501cfcef71bd1487996a035
RodrigoMato00 <3293@holbertonschool.com>
2021-06-29 09:32:20 -0700

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,4 +5,10 @@
 /**
  * main - generates random valid passwords for the program 101-crackme
  * Description: generates random valid passwords for the program 101-crackme
+ * Return "0"
  */
+
+int main(void)
+{
+return (0);
+}
