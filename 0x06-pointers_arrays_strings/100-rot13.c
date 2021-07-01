4b92d6a373fc0575d02d491ac39b4dcbbb84f9fd
RodrigoMato00 <3293@holbertonschool.com>
2021-07-01 13:38:05 -0700

    hola

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,7 +14,7 @@ char *rot13(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
+		if ((s[i] > 64 && s[i] < 92) || (s[i] > 96 && s[i] < 124))
 		{
 			s[i] = (s[i] - 65 > 25) ?
 				storel [s[i] - 97] : storeh[s[i] - 65];
