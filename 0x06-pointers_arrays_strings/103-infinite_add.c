4d8d15e9a0261bb1d7c88903e023ef147585fa76
RodrigoMato00 <3293@holbertonschool.com>
2021-07-01 20:00:50 -0700

    hola

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,55 +1,53 @@
-#include "holberton.h"
-
+ #include "holberton.h"
 /**
- * infinite_add - Add numbers
- * Description: This function add two numbers
- * @n1: First number to add
- * @n2: Second number to add
- * @r: Buffer that stpre the result
- * @size_r: Size of the buffer
- * Return: The result of @n1 + @n2, if the result < @r
+ * infinite_add -  adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: result
+ * @size_r: result lenght
+ * Return: sum
+ *
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
 {
-	int c = 0, c1 = 0, c2 = 0, acum = 0;
 
-	size_r--;
+	int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
-	while (*(n1 + c1) != '\0' || *(n2 + c2) != '\0')
-	{
-		if (*(n1 + c1) != '\0')
-			c1++;
-		if (*(n2 + c2) != '\0')
-			c2++;
-	}
-	c1--;
-	c2--;
-	while (c1 >= 0 && c2 >= 0)
-	{
-		*(r + c) = ((*(n1 + c1) + *(n2 + c2) - 96 + acum) % 10) + 48;
-		acum = ((*(n1 + c1) + *(n2 + c2) - 96) / 10);
-		c++;
-		c1--;
-		c2--;
-	}
-	while (c1 >= 0)
-	{
-		*(r + c) = ((*(n1 + c1) - 48 + acum) % 10) + 48;
-		acum = ((*(n1 + c1) - 48 + acum) / 10);
-		c++;
-		c1--;
-	}
-	while (c2 >= 0)
-	{
-		*(r + c) = ((*(n2 + c2) - 48 + acum) % 10) + 48;
-		acum = ((*(n2 + c2) - 48 + acum) / 10);
-		c++;
-		c2--;
-	}
-	*(r + c) = acum + 48;
-	if (c >= size_r)
-	{
+	while (n1[i] != '\0')
+		i++;
+	while (n2[j] != '\0')
+		j++;
+	if (i > j)
+		l = i;
+	else
+		l = j;
+	if (l + 1 > size_r)
 		return (0);
+	r[l] = '\0';
+	for (k = l - 1 ; k >= 0 ; k--)
+	{
+		i--;
+		j--;
+		if (i >= 0)
+			f = n1[i] - '0';
+		else
+			f = 0;
+		if (j >= 0)
+			s = n2[j] - '0';
+		else
+			s = 0;
+		r[k] = (f + s + d) % 10 + '0';
+		d = (f + s + d) / 10;
+	}
+	if (d == 1)
+	{
+		r[l + 1] = '\0';
+		if (l + 2 > size_r)
+			return (0);
+		while (l-- >= 0)
+			r[l + 1] = r[l];
+		r[0] = d + '0';
 	}
 	return (r);
 }
