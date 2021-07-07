d09d52d4ed231ae7c752ad6babc093058821dcd3
RodrigoMato00 <3293@holbertonschool.com>
2021-07-07 06:19:43 -0700

    0x08-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *_puts_recursion - print a string
+ *@s is a string
+ */
+void _puts_recursion(char *s);
+{
+	if (*s == '\n')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		puts_recursion(++s);
+	}
+}
