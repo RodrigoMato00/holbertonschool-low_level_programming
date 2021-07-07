e4f6eeca0ad80579c5e2067b8115b56c93104064
RodrigoMato00 <3293@holbertonschool.com>
2021-07-07 08:43:16 -0700

    0x08-recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,9 +8,13 @@
 int factorial(int n)
 {
 	if (n < 0)
+	{
 		return (-1);
+	}
 	else if (n < 2)
-		     return (n);
+	{
+		return (n);
+	}
 	else
-		return (n * factorial(n -1));
+		return (n * factorial(n - 1));
 }
