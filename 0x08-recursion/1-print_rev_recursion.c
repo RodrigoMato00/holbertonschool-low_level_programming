41f938b5293d183570963c8f06f8500730ace232
RodrigoMato00 <3293@holbertonschool.com>
2021-07-07 06:47:49 -0700

    0x08-recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *_print_rev_recursion - print string
+ *@s: is a string
+ */
+void _print_rev_recursion(char *s)
+{
+	if(*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
