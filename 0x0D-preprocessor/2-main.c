fb4efbc8a963f2cb794e1a6b67f829f37f0c968d
RodrigoMato00 <3293@holbertonschool.com>
2021-07-19 07:50:31 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Ptint a name of file compiled
+ *Return: 0;
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef TREE_FUCTION_LIKE_MACRO_H
+#define TREE_FUCTION_LIKE_MACRO_H
+#define ABS(x) (x > 0 ? (x) : (x) * -1)
+#endif
