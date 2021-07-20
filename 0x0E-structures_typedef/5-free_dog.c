d93598beb7a272ea074a5c5ffcf90f2594bc3c9c
RodrigoMato00 <3293@holbertonschool.com>
2021-07-19 17:00:10 -0700

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *free_dog - free dog
+ *Description: free a dog
+ *@d: dog structure
+ */
+void free_dog(dog_t *d)
+{
+	dog_t *my_dog =d;
+
+	if (d)
+	{
+		free(my_dog->name);
+		free(my_dog->owner);
+		free(my_dog);
+	}
+}
