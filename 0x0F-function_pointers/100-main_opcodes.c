ec544d146eb80bb0688c45347247b8f4fb9128b4
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2021-07-22 09:53:22 -0300

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,1 +1,37 @@
-#includexx
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry function
+ * Description: This function prints the opcodes of its own main function
+ * @argc: Number of parameters
+ * @argv: Array of parameters
+ * Return: 1 if @argc != 2, 2 if atoi of @argv[1] is negative, 0 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int i, value;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	value = atoi(argv[1]);
+	if (value < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < value; i++)
+	{
+		printf("%02hhx", *((unsigned char *) main + i));
+		if (i < value - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
