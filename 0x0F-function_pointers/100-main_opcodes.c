b3efd79c65b7172e4fedaffe5259007a43eaafc4
RodrigoMato00 <3293@holbertonschool.com>
2021-07-22 05:54:55 -0700

    100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,1 +1,18 @@
-#includexx
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - main fuction
+ *@argc: numbers parameters
+ *argv: array parameters
+ *return: return my fuction
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int val;
+
+	if (argc != 2)
+	{
+		printf("error\n")
+			exit(1);
