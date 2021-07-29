c8757911a589cd0e7c89c5ef38f35602bc22799b
RodrigoMato00 <3293@holbertonschool.com>
2021-07-29 10:05:05 -0700

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,8 +2,23 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ *print_list - print all elements of a llist_t
+ *@h: head
+ *Return: node
  */
 size_t print_list(const list_t *h)
+{
+
+	const list_t *jeje;
+	size_t  a;
+
+	a = 0;
+	jeje = h;
+	while (jeje != NULL)
+	{
+		printf("[%d] %s\n", jeje ->len, jeje->str);
+	        jeje = jeje->next;
+		a ++;
+	}
+	return (a);
+}
