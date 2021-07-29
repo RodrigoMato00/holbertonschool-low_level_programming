36dd618252a829b3b601212f32a20e947528137f
RodrigoMato00 <3293@holbertonschool.com>
2021-07-29 12:30:12 -0700

    2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ *add_node - adds a new node at the beginning of a list_t
+ *@head: head
+ *@str: str
+ *Return: algo
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	char *x;
+	int a;
+	list_t *jeje;
+
+	jeje = malloc(sizeof(list_t));
+	if (jeje == NULL)
+		return (NULL);
+
+	x = strdup(str);
+	if (x == NULL)
+	{
+		free(jeje);
+	        return (NULL);
+	}
+
+	for (a = 0; str[a];)
+		a++;
+
+	jeje->str = x;
+	jeje->len = a;
+	jeje->next = *head;
+
+	*head = jeje;
+
+	return (jeje);
+}
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,9 @@
+#include <string.h>
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
