db920012deb2c056ca977d62121b901ab9c64ca4
RodrigoMato00 <3293@holbertonschool.com>
2021-07-29 13:02:37 -0700

    3final

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,8 +2,45 @@
 #include "lists.h"
 
 /**
- *
- *
- *
- *
+ *add_node_end - adds a new node at the end of a list_t
+ *@head: head
+ *@str: str
+ *Return: head
  */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	char *x;
+	int a;
+	list_t *new, *last;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	x = strdup(str);
+	if (x == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (a = 0; str[a];)
+		a++;
+
+	new->str = x;
+	new->len = a;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+
+		last->next = new;
+	}
+	return (*head);
+}
