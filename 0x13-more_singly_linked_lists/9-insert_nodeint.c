2e3fcaee2c7d8bbf80422b2702bde4b54868bb2b
RodrigoMato00 <3293@holbertonschool.com>
2021-08-03 10:12:35 -0700

    correccion final 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (node < idx)
 	{
 		free(new_node);
-	        return (NULL);
+		return (NULL);
 	}
 
 	co = *head;
