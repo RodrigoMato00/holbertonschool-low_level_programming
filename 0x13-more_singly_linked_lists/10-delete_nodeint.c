8238087fedaffb285ba98b8cacd9d1265e0b919e
RodrigoMato00 <3293@holbertonschool.com>
2021-08-03 11:15:27 -0700

    10 primer intento

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ *delete_nodeint_at_index - deletes the node at index index of a linked list
+ *@head: address of the linked list
+ *@index: node deleted
+ *Return: 1 is success, -1 fail.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *h;
+	listint_t *c;
+
+	i = 0;
+	c = *head;
+	if (*head == NULL)
+		return (-1);
+
+	while (i < index)
+	{
+		i++;
+		h = c;
+		if (c->next)
+			c = c->next;
+		else
+			return (-1);
+	}
+
+	if (index == 0)
+		*head = c->next;
+	else if (c->next)
+		h->next = c->next;
+	else
+		h->next = NULL;
+
+	free(c);
+	return (-1);
+}
