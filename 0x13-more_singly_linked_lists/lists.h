f6221333e8679fc4f4346f6a8a6558357b0ca66f
RodrigoMato00 <3293@holbertonschool.com>
2021-08-03 15:09:23 -0700

    0 arreglado

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,17 +6,17 @@
 #include <stdio.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ *struct listint_s - singly linked list
+ *@n: integer
+ *@next: points to the next node
  *
- * Description: singly linked list node structure
+ *Description: singly linked list node structure
  *
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -35,4 +35,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif 
+#endif
