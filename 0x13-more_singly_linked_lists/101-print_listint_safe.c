6720895ebf66e7f6d50439748d3320823dff016d
RodrigoMato00 <3293@holbertonschool.com>
2021-08-04 12:00:29 -0700

    checker puto3

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -15,7 +15,7 @@ size_t print_listint_safe(const listint_t *head)
 	v = 0;
 	t = head;
 
-	if (head == NULL || head->nex == NULL)
+	if (head == NULL || head->next == NULL)
 		exit(98);
 
 	while (t)
