7f69cce39b60f1eb849ffebab9d8f423ba479348
RodrigoMato00 <3293@holbertonschool.com>
2021-08-09 11:08:55 -0700

    ahora si? 21

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_APPEND | O_WRONLY);
+	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
 		return (-1);
 
