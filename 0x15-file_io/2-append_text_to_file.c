47f3e50cc0fbd07c4b6e1fc2b2f263f2eb2f2f67
RodrigoMato00 <3293@holbertonschool.com>
2021-08-09 10:54:42 -0700

    ahora si?

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ *append_text_to_file - text append
+ *@filename: file name
+ *@text_content: write file
+ *Return: 1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+	int fd;
+	int sta;
+	int a;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_APPEND | O_WRONLY);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	for (a = 0; text_content[a] != '\0'; a++)
+		;
+
+	sta = write(fd, text_content, a);
+	if (sta == -1)
+	{
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
