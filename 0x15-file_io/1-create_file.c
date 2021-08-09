bc83a1e243de7f5d3128ad9331c29fcd9840a64f
RodrigoMato00 <3293@holbertonschool.com>
2021-08-09 10:09:31 -0700

    correccion 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,24 +9,34 @@
 int create_file(const char *filename, char *text_content)
 {
 	int a, b, c;
+	char *buf;
 
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+	{
+		a = open(filename, O_CREAT, 0600);
+		if (a == -1)
+			return (-1);
+		return (1);
+	}
 
-	a = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	for (c = 0; text_content[c] != '\0'; c++)
+		;
+	buf = malloc(c * sizeof(char));
+	if (buf == NULL)
+		return (-1);
 
+	a = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (a == -1)
 		return (-1);
 
-	if (text_content)
-	{
-		for (c = 0; text_content[c] != '\0'; c++)
-			;
-		b = write(a, text_content, c);
-			if (b == -1)
-				return (-1);
-	}
+	b = write(a, text_content, c);
+	if (b == -1)
+		return (-1);
+
 	close(a);
-	return (-1);
+	free(buf);
+	return (1);
 }
