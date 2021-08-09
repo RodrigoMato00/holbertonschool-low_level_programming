420350e3a767da89e957a4519cb4712b9b5e8292
RodrigoMato00 <3293@holbertonschool.com>
2021-08-09 12:16:05 -0700

    hola

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -8,12 +8,3 @@
  *Return: return 0
  */
 int main(int argc, char *argv[])
-{
-	if (argc != 2)
-	{
-		dprintf(STDERR_FILENO, "usage: elf_header elf_filename\n");
-			exit(98);
-	}
-	(void)argv;
-	return (0);
-}
