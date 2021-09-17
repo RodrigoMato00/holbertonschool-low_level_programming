98b61f354c370c668196978f26228e6d0868f972
rodrigomato00 <3293@holbertonschool.com>
2021-09-17 22:55:02 +0000

    7 terminado

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,60 @@
+#include "lists.h"
+
+/**
+ *insert_dnodeint_at_index - wiojgiowfiwofjiowjgiowjrg
+ *@h: iowjdgwiopgjiopeg
+ *@idx: owhfuowienhfouiwenfonwf
+ *@n: ieorjgioemjeriogeiormgemiog
+ *Return: kidgjiopsgmkifio
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *nuevo;
+	dlistint_t *siguiente;
+	dlistint_t *anterior;
+	unsigned int a = 1;
+
+	nuevo = malloc(sizeof(dlistint_t));
+	if (nuevo == NULL)
+		return (NULL);
+
+	nuevo->n = n;
+
+	if (*h == NULL)
+	{
+		nuevo->prev = NULL;
+		nuevo->next = NULL;
+		*h = nuevo;
+		return (nuevo);
+	}
+
+	anterior = *h;
+	if (idx == 0)
+	{
+		*nuevo = *add_dnodeint(h, n);
+		return (nuevo);
+
+	}
+	while (a < idx && anterior != NULL)
+	{
+		anterior = anterior->next;
+		a++;
+	}
+
+	if (anterior == NULL && a < idx)
+		return (NULL);
+
+	if (anterior == NULL && a == idx)
+	{
+		*nuevo = *add_dnodeint_end(h, n);
+		return (nuevo);
+	}
+
+	siguiente = anterior->next;
+	anterior->next = nuevo;
+	nuevo->prev = anterior;
+	nuevo->next = siguiente;
+	nuevo = *h;
+
+	return (nuevo);
+}
