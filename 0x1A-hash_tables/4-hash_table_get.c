dc38f7ab1245a1247af0ae1551661103fe4304c0
rodrigomato00 <3293@holbertonschool.com>
2021-10-07 17:52:45 +0000

    4 terminado

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -3,8 +3,29 @@
 /**
  *hash_table_get - bbbbbbbbbb
  *@ht: odsijfs
- *@Key: dsjfskdlfm
+ *@key: dsjfskdlfm
  *Return: dijfsdpojf
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int ind;
+	hash_node_t *buscador;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	ind = key_index((const unsigned char *)key, ht->size);
+
+	if (ht->array[ind] == NULL)
+		return (NULL);
+
+	buscador = ht->array[ind];
+
+	while (buscador)
+	{
+		if (strcmp(buscador->key, key) == 0)
+			return (buscador->value);
+		buscador = buscador->next;
+	}
+	return (NULL);
+}
