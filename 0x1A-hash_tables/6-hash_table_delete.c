979e46a5125eb7ecc386d39ffea65b139572dc6f
rodrigomato00 <3293@holbertonschool.com>
2021-10-08 23:00:54 +0000

    6terminado

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+/**
+ *hash_table_delete - borrar la hash table
+ *@ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *nodo;
+	unsigned long int corredor;
+
+	corredor = 0;
+
+	if (ht == NULL)
+		return;
+
+	while (corredor < ht->size)
+	{
+
+		while (ht->array[corredor] != NULL)
+		{
+			nodo = ht->array[corredor];
+			free(nodo->key);
+			free(nodo->value);
+			ht->array[corredor] = ht->array[corredor]->next;
+			free(nodo);
+		}
+		corredor++;
+	}
+	free(ht->array);
+	free(ht);
+}
