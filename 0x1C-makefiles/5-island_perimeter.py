7f634b65142d2ed899877374e43990f4330d5a7d
RodrigoMato00 <3293@holbertonschool.com>
2021-11-25 10:33:38 -0800

    5 arreglo

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,12 +11,8 @@ grid is a list of list of integers:
 
 
 def island_perimeter(grid):
-    """ grid is a list of list of integers:
-    -0 represents a water zone
-    -1 represents a land zone
-    -One cell is a square with side length 1
-    -Grid cells are connected horizontally/vertically (not diagonally).
-    -Grid is rectangular, width and height don’t exceed 10
+    """
+    island_perimeter
     """
     perimeter = 0
     for i in range(len(grid)):
