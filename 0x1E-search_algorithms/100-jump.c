10cf2b69f7c382a4cd77893c10f37dd2f3975704
#RodrigoMato00 <3293@holbertonschol.com>
2022-01-17 17:30:11 -0300

    agregue print value checked array[0] = [0]

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -18,7 +18,7 @@ int jump_search(int *array, size_t size, int value)
     pos = 0;
 
     printf("Value checked array[%lu] = [%d]\n", a + pos, array[a + pos]);
-    //print value checked array[0] = [0]
+    /*print value checked array[0] = [0]*/
 
     while (pos + a && value > array[a + pos])
     {
