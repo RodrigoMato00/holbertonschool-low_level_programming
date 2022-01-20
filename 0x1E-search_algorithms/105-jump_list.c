0fb2cdc51f70835d1c1407a081e603ccdfac1ae1
Rodrigomato00 <3293@holbertonschool.com>
2022-01-19 23:33:19 -0300

    105 correccion 106 primer checker

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,15 +1,15 @@
 #include "search_algos.h"
 
 /**
- *jump_list - searches for a value in an array
+ *muv_list - searches for a value in an array
  *@list: pointer to the first element of the array
  *@value: value to search for
  *@size: size of the array
  *Return: index of the value or -1 if not found
  **/
-listint_t *jump_list(listint_t *list, size_t size, int value)
+listint_t *muv_list(listint_t *list, size_t size, int value)
 {
-	listint_t *a, *jump;
+	listint_t *a, *muv;
 	size_t pos, nsize;
 
 	if (!list)
@@ -18,22 +18,22 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 	pos = 0;
 	nsize = sqrt(size);
 
-	for (a = jump = list; jump->index + 1 < size && jump->n < value;)
+	for (a = muv = list; muv->index + 1 < size && muv->n < value;)
 	{
-		a = jump;
-		for (pos += nsize; jump->index < pos; jump = jump->next)
+		a = muv;
+		for (pos += nsize; muv->index < pos; muv = muv->next)
 		{
-			if (jump->index + 1 == size)
+			if (muv->index + 1 == size)
 				break;
 		}
 
-		printf("Value checked at index [%li] = [%i]\n", jump->index, jump->n);
+		printf("Value checked at index [%li] = [%i]\n", muv->index, muv->n);
 	}
 
 	printf("Value found between indexes [%li] and [%li]\n", a->index,
-		jump->index);
+		muv->index);
 
-	for (; a->index < jump->index && a->n < value; a = a->next)
+	for (; a->index < muv->index && a->n < value; a = a->next)
 		printf("Value checked at index [%ld] = [%d]\n", a->index, a->n);
 	printf("Value checked at index [%li] = [%i]\n", a->index, a->n);
 
---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+
+/**
+ *linear_skip - linear search algorithm
+ *@array: array to search
+ *@size: size of array
+ *@value: value to search for
+ *Return: index of value or -1 if not found
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+    skiplist_t *a, *muv;
+
+	if (!list)
+		return (NULL);
+
+	for (a = muv = list; muv->next != NULL && muv->n < value;)
+	{
+		a = muv;
+		if (muv->express != NULL)
+		{
+			muv = muv->express;
+			printf("Value checked at index [%ld] = [%d]\n",
+					muv->index, muv->n);
+		}
+
+        else
+		{
+			for (; muv->next != NULL;)
+				muv = muv->next;
+		}
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			a->index, muv->index);
+
+	for (; a->index < muv->index && a->n < value;  a = a->next)
+		printf("Value checked at index [%ld] = [%d]\n", a->index, a->n);
+
+    printf("Value checked at index [%ld] = [%d]\n", a->index, a->n);
+
+	return (a->n == value ? a : NULL);
+}
